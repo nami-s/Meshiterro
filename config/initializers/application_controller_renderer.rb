d4a263b14c1b37bc465410fa12a17dccba07e3a1
nami-s <na.s1222.10feettt@gmail.com>
2020-12-13 05:06:22 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e0b5b0ef10c9d22fd68166a57ed82b18d351e90a272849b44ddbc7a617161f7d1a9f5b845c20b1bf685d8fb2f4a52376488be336b8904927a3dba4ea56df4c45'
